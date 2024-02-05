024e4583ef58d086e81823d5f9985e2985b2b2e0
willy15662 <121214614+willy15662@users.noreply.github.com>
2024-02-05 15:33:57 +0800

    Update 125. Valid Palindrome_easy.cpp

--- Two Pointers/125. Valid Palindrome_easy.cpp
+++ Two Pointers/125. Valid Palindrome_easy.cpp
@@ -19,6 +19,26 @@ public:
     }
 };
 
+/* //速解  用isalnum函式
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+}
+};
+*/
+
 
 /*
 A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
