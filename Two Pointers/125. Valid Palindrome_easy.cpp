5ef802bb64008211bf7a42c4ed2a172806d42668
willy15662 <121214614+willy15662@users.noreply.github.com>
2024-02-05 15:32:45 +0800

    Create 125. Valid Palindrome_easy.cpp

---
+++ Two Pointers/125. Valid Palindrome_easy.cpp
@@ -0,0 +1,46 @@
+/*先將非字母及數字的字元去掉
+左右各放一個指標，依序比對直到兩指標交會
+*/
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string output;
+        for(char c : s){
+            if('0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+                output += tolower(c);
+        }
+        int right = 0, left = output.size()-1;
+        while(right < left){
+            if(output[right++] != output[left--]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+*/
